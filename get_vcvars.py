30b79bfe9cb23adaaec99eed962c7e0a58e8987a
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2018-02-10 19:56:04 +0300

    get_vcvars (normal)

--- get_vcvars.py
+++ get_vcvars.py
@@ -1,2 +1,13 @@
-def fake():
-    pass
+def find_installation_paths():
+    return {}
+
+def select_vcvarsall(installation_paths):
+    return "", []
+
+def get_environment_variables(vcvarsall, args):
+    return {}
+    
+def get_vcvars(settings):
+    installation_paths = find_installation_paths()
+    vcvarsall, args = select_vcvarsall(settings, installation_paths)
+    return get_environment_variables(vcvarsall, args)
--- tests/test_get_vcvars.py
+++ tests/test_get_vcvars.py
@@ -1,11 +1,58 @@
 import unittest
-#from unittest.mock import patch
+from unittest.mock import patch
 
 
 import get_vcvars
+from conans.model.settings import Settings
 
 
 class Test_get_vcvars(unittest.TestCase):
 
-    def test_fake(self):
-        pass
+    @patch("get_vcvars.get_environment_variables")
+    @patch("get_vcvars.select_vcvarsall")
+    @patch("get_vcvars.find_installation_paths")
+    def test_normal(self, mock_find_installation_paths, mock_select_vcvarsall, mock_get_environment_variables):
+        mock_find_installation_paths.return_value = {
+            "15.5.27130.2026": "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community",
+            "14.0": "C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\"
+        }
+        mock_select_vcvarsall.return_value = [
+            "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\VC\\Auxiliary\\Build\\vcvarsall.bat",
+            ["amd64"]
+        ]
+        mock_get_environment_variables.return_value = {
+            "INCLUDE": "C:\\include",
+            "PATH": "C:\\bin"
+        }
+        settings = Settings(definition={
+            "arch": ["x86", "x86_64"],
+            "compiler": {
+                "Visual Studio": {
+                    "version": [14, 15]
+                }
+            }
+        })
+        settings.arch = "x86_64"
+        settings.compiler = "Visual Studio"
+        settings.compiler.version = "15"
+
+        result = get_vcvars.get_vcvars(settings)
+        
+        self.assertEqual(result, {
+            "INCLUDE": "C:\\include",
+            "PATH": "C:\\bin"
+        })
+
+        mock_find_installation_paths.assert_called_once_with()
+        mock_select_vcvarsall.assert_called_once_with(settings, {
+            "15.5.27130.2026": "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community",
+            "14.0": "C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\"
+        })
+        mock_get_environment_variables.assert_called_once_with(
+            "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\VC\\Auxiliary\\Build\\vcvarsall.bat",
+            ["amd64"]
+        )
+        
+
+if __name__ == "__main__":
+    unittest.main()
