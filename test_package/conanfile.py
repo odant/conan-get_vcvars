3d89f9b0e928a226da6ff14db013c70a918f5048
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2018-02-11 23:20:27 +0300

    Appvoyer, 5

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -4,13 +4,16 @@ from conans import ConanFile, tools
 class TestPackage(ConanFile):
     settings = {"os": ["Windows"], "arch": ["x86", "x86_64"]}
 
+    def build(self):
+        import get_vcvars
+        self.output.info("--------Visual Studio enviroment--------")
+        self.output.info("arch: %s compiler.version: %s" % (self.setting.arch, self.settings.compiler.version))
+        env_vcvars = get_vcvars.get_vcvars(self.settings)
+        for key, value in env_vcvars.items():
+            self.output.info("%s :" % key)
+            for split_value in value.split(";"):
+                self.output.info(split_value)
+
     def test(self):
-        with tools.pythonpath(self):
-            import get_vcvars
-            self.output.info("--------Variable for SDK--------")
-            env_vcvars = get_vcvars.get_vcvars(self.settings)
-            for key, value in env_vcvars.items():
-                self.output.info("%s :" % key)
-                for split_value in value.split(";")
-                    self.output.info(split_value)
+        pass
 
