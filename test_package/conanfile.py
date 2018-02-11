ecaa4ee33548e35e0cd1d9058f4580a01a7db374
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2018-02-11 23:26:44 +0300

    Appvoyer, 6

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -5,14 +5,15 @@ class TestPackage(ConanFile):
     settings = {"os": ["Windows"], "arch": ["x86", "x86_64"]}
 
     def build(self):
-        import get_vcvars
-        self.output.info("--------Visual Studio enviroment--------")
-        self.output.info("arch: %s compiler.version: %s" % (self.setting.arch, self.settings.compiler.version))
-        env_vcvars = get_vcvars.get_vcvars(self.settings)
-        for key, value in env_vcvars.items():
-            self.output.info("%s :" % key)
-            for split_value in value.split(";"):
-                self.output.info(split_value)
+        with tools.pythonpath(self):
+            import get_vcvars
+            self.output.info("--------Visual Studio enviroment--------")
+            self.output.info("arch: %s compiler.version: %s" % (self.setting.arch, self.settings.compiler.version))
+            env_vcvars = get_vcvars.get_vcvars(self.settings)
+            for key, value in env_vcvars.items():
+                self.output.info("%s :" % key)
+                for split_value in value.split(";"):
+                    self.output.info(split_value)
 
     def test(self):
         pass
